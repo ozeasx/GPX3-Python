7556e223e279cabcaf686d15969bccfa1f1f14a1
ozeasx <ozeasx@gmail.com>
2019-03-12 17:00:51 -0300

    Commit before testing 4

--- R/gen_report_data.r
+++ R/gen_report_data.r
@@ -12,19 +12,11 @@ report_file = paste(args[1], "report1.log", sep="")
 instance_file = grep(".tsp", readLines(report_file), value=TRUE)
 instance_file = strsplit(instance_file, " ")[[1]][2]
 
-
 # Args size
 n = length(args)
 
 # Load TSP instance
 tsp = read_TSPLIB(instance_file)
-
-# Load best known tour
-best_known_tour_file = paste(args[n], "best_known_tour.out", sep = '')
-best_known_tour = scan(best_known_tour_file, sep = ',')
-# https://stackoverflow.com/questions/5665599/range-standardization-0-to-1-in-r
-range0b <- function(x){(x-min(x))/(-tour_length(best_known_tour, tsp)-min(x))}
-rangeMm <- function(x, M, m){(x-m)/(M-m)}
 
 # Generate various file paths
 param_files = c()
@@ -41,29 +33,30 @@ for (i in 1:n) {
   best_tour_files[i] = paste(args[i], "best_tour_found.out", sep="")
 }
 
-# Choose best tour found
-for (i in 1:n) {
-  tour = TOUR(scan(best_tour_files[i], sep = ','))
-  if (!exists("best_tour")) {
-    best_tour = tour
-  } else if (tour_length(tour, tsp) < tour_length(best_tour, tsp)) {
-    best_tour = tour
-  }
-}
-
-# Plot best_tour found
-tour_plot_file = paste(args[1], "tours.png", sep = '')
-png(tour_plot_file, width=1024, height=1024)
-par(pty="s")
-plot(tsp, best_tour, asp = 1, xlab="x", ylab="y", main=tour_length(best_tour))
-dev.off()
+# # Choose best tour found
+# for (i in 1:n) {
+#   tour = TOUR(scan(best_tour_files[i], sep = ','))
+#   if (!exists("best_tour")) {
+#     best_tour = tour
+#   } else if (tour_length(tour, tsp) < tour_length(best_tour, tsp)) {
+#     best_tour = tour
+#   }
+# }
+#
+# # Plot best_tour found
+# tour_plot_file = paste(args[1], "tours.png", sep = '')
+# png(tour_plot_file, width=1024, height=1024)
+# par(pty="s")
+# plot(tsp, best_tour, asp = 1, xlab="x", ylab="y", main=tour_length(best_tour))
+# dev.off()
 
 # Get parametrization
 trim <- function (x) gsub("\\s+", " ", str_trim(x))
 default_params = c("k: 3", "P: None", "K: None", "t1: None", "t2: None",
-                   "t3: None", "t1f: None", "t2f: None", "t3f: None", "p: 100",
+                   "t3: None", "t1f: True", "t2f: False", "t3f: False", "p: 100",
                    "M: random", "R: 1.0", "r: 0.5", "S: random", "e: 0",
-                   "c: 1.0", "m: 0", "t: 2opt", "G: False", "o: None")
+                   "c: 1.0", "m: 0", "t: 2opt", "G: False", "o: None",
+                   "E: True", "F: False")
 
 params = lapply(param_files, scan, sep = ',', what = "list")
 params = lapply(params, trim)
@@ -98,8 +91,6 @@ params = lapply(params, paste, collapse = ", ")
 fitness = lapply(fitness_files, read.csv2, sep = ',', dec = '.',
                  header = FALSE)
 fitness = lapply(fitness, rowMeans)
-# fitness = lapply(fitness, rangeMm, max(sapply(fitness, max)),
-#                                    min(sapply(fitness, min)))
 
 # Plot fitness
 # Change the palette to avoid yellow
@@ -119,8 +110,7 @@ plot(fitness[[1]], type = 'n', xlab = "Generation", ylab = "Fitness",
 for (i in 1:n) {
   lines(fitness[[i]], type = 'l', lty = linetype[i], col = colors[i])
 }
-legend(10, -2500, params,
-       lty = linetype, col=colors)
+legend(10, -7500000, params, lty = linetype, col=colors)
 dev.off()
 
 # Summarize data
--- python/batch.sh
+++ python/batch.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-./main.py -c 1 -r 0.5 -k 3 ../tsplib/vm1084.tsp -t1 1 -t2 0 -t3 0 -F False -g 1000 -n 30 -o ../results3/TFF
-./main.py -c 1 -r 0.5 -k 3 ../tsplib/vm1084.tsp -t1 0 -t2 1 -t3 0 -F False -g 1000 -n 30 -o ../results3/FTF
-./main.py -c 1 -r 0.5 -k 3 ../tsplib/vm1084.tsp -t1 0 -t2 0 -t3 1 -F False -g 1000 -n 30 -o ../results3/FFT
+./main.py -c 1 -r 0.5 -k 3 ../tsplib/u1060.tsp -t1 1 -t2 0 -t3 0 -F False -g 1000 -n 30 -o ../results4/TFF
+./main.py -c 1 -r 0.5 -k 3 ../tsplib/u1060.tsp -t1 0 -t2 1 -t3 0 -F False -g 1000 -n 30 -o ../results4/FTF
+./main.py -c 1 -r 0.5 -k 3 ../tsplib/u1060.tsp -t1 0 -t2 0 -t3 1 -F False -g 1000 -n 30 -o ../results4/FFT
 
-./main.py -c 1 -r 0.5 -k 3 ../tsplib/vm1084.tsp -t1 1 -t2 1 -t3 0 -F False -g 1000 -n 30 -o ../results3/TTF
-./main.py -c 1 -r 0.5 -k 3 ../tsplib/vm1084.tsp -t1 1 -t2 0 -t3 1 -F False -g 1000 -n 30 -o ../results3/TFT
-./main.py -c 1 -r 0.5 -k 3 ../tsplib/vm1084.tsp -t1 0 -t2 1 -t3 1 -F False -g 1000 -n 30 -o ../results3/FTT
+./main.py -c 1 -r 0.5 -k 3 ../tsplib/u1060.tsp -t1 1 -t2 1 -t3 0 -F False -g 1000 -n 30 -o ../results4/TTF
+./main.py -c 1 -r 0.5 -k 3 ../tsplib/u1060.tsp -t1 1 -t2 0 -t3 1 -F False -g 1000 -n 30 -o ../results4/TFT
+./main.py -c 1 -r 0.5 -k 3 ../tsplib/u1060.tsp -t1 0 -t2 1 -t3 1 -F False -g 1000 -n 30 -o ../results4/FTT
 
-./main.py -c 1 -r 0.5 -k 3 ../tsplib/vm1084.tsp -t1 1 -t2 1 -t3 1 -F False -g 1000 -n 30 -o ../results3/TTT
+./main.py -c 1 -r 0.5 -k 3 ../tsplib/u1060.tsp -t1 1 -t2 1 -t3 1 -F False -g 1000 -n 30 -o ../results4/TTT
